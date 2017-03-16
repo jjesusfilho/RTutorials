481d7fe01e42144ed8007b3de4ae33dc9bc05f24
Erin Grand <elg2158@columbia.edu>
2017-03-16 18:44:36 -0400

    alsmot done with dplyr slides

--- setup.R
+++ setup.R
@@ -1,3 +1,4 @@
+  
 #Download packages and datasets
 list.of.packages <- c("nycflights13", 
                       "gapminder", 
@@ -15,3 +16,45 @@ if(length(new.packages) > 0) install.packages(new.packages)
 Sys.setenv(LANGUAGE = "en")
 
 
+# library(readxl)
+# 
+# enrollment <- "S:/Data Analytics/Resource Library/R Help/Assignment/Enrollment.xlsx"
+# 
+# # Define A Load Data Functions
+# loaddata <- function(file, sheetname, cn){
+#   x <- read_excel(file, sheet = sheetname, na = "", col_names = cn)
+#   valid_column_names <- make.names(names=names(x), unique=TRUE, allow_ = TRUE)
+#   names(x) <- valid_column_names
+#   return(x)
+# }
+# 
+# # Load the Data (fill in these commands)
+# lastyearEnrollment <- loaddata(enrollment, 1, T)
+# thisyearEnrollment <- loaddata(enrollment, 2, T)
+# schoolMap <- loaddata(enrollment, 3, T)
+# 
+# 
+# IAdata <- "S:/Data Analytics/Interim Assessments/SY15-16/Source files/Tableau/Raw Data_v1_SY1516_upload.xlsx"
+# coltypes <- rep("text", 23)
+# coltypes[c(13, 20, 21)] <- "numeric"
+# IAs <- read_excel(IAdata, "Student IA Results", col_names=T, col_types=coltypes)
+# valid_column_names <- make.names(names=names(IAs), unique=TRUE, allow_ = TRUE)
+# names(IAs) <- valid_column_names
+# 
+# assessMap <- loaddata(IAdata, "Assessment mapping", T) %>%
+#   select(Assessment.Name:Year)
+# 
+setwd("S:/Data Analytics/Resource Library/R Help/data")
+
+# saveRDS(IAs, "IAs.rds")
+# saveRDS(assessMap, "assessMap.rds")
+# saveRDS(schoolMap, "schoolMap.rds")
+# saveRDS(lastyearEnrollment, "lastyearEnrollment.rds")
+# saveRDS(thisyearEnrollment, "thisyearEnrollment.rds")
+
+
+IAs <- readRDS("IAs.rds")
+assessMap <- readRDS("assessMap.rds")
+schoolMap <- readRDS("schoolMap.rds")
+lastyearEnrollment <- readRDS("lastyearEnrollment.rds")
+thisyearEnrollment <- readRDS("thisyearEnrollment.rds")
