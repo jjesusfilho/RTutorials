bf928a3248d84dc5e9c67420797883b68ebfeaed
Erin Grand <elg2158@columbia.edu>
2017-03-16 15:17:54 -0400

    so many things

--- setup.R
+++ setup.R
@@ -1,6 +1,17 @@
 #Download packages and datasets
-install.packages(c("nycflights13", "gapminder", "Lahman"))
-install.packages(c("tidyverse", "broom"))
+list.of.packages <- c("nycflights13", 
+                      "gapminder", 
+                      "Lahman", 
+                      "tidyverse", 
+                      "readxl", 
+                      "fivethirtyeight", 
+                      "broom")
+
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages) > 0) install.packages(new.packages)
+
 
 # set system langaue to english, just in case
 Sys.setenv(LANGUAGE = "en")
+
+
